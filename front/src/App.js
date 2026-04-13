9c11d9ca891f738a30808add291f2cc666637d8e
CHANGJeremyVinh-PhongKram-Tian <jeremy.chang@lmdsio.fr>
2026-04-13 15:02:00 +0200

    matrix

--- front/src/App.js
+++ front/src/App.js
@@ -1,11 +1,18 @@
 import React from "react";
 import Matrix from "./component/Matrix";
+import "./App.css"; // Import du style global (fond noir)
+import "./css/Matrix.css"; // Import du style de la matrice
 
 export default function App() {
   return (
-    <div>
-      <h1>Matrice</h1>
-      <Matrix rows={3} cols={3} />
+    <div className="App">
+      <div className="App-header">
+        {/* Titre stylisé en vert */}
+        <h1 className="main-title">PROJET <span className="highlight">CRA</span></h1>
+        
+        {/* Ta matrice est automatiquement centrée par le CSS de App-header */}
+        <Matrix rows={5} /> 
+      </div>
     </div>
   );
 }
